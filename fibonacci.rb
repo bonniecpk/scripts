24250cb70dc95d51f90d35fbee04485e75c80665
bonniecpk <poki.developer@gmail.com>
2013-09-28 18:40:48 -0400

    reading from stein

--- fibonacci.rb
+++ fibonacci.rb
@@ -6,20 +6,23 @@ def fibonacci(term)
   return fibonacci(term - 2) + fibonacci(term - 1)
 end
 
-puts fibonacci(4)
+#puts fibonacci(7)
 
 
 
 # Fibonacci sequence:
 # 1, 1, 2, 3, 5, 8, 13....
+input = STDIN.read.to_i
+#input = ARGF.read.first
+
 @@cache = {1 => 1, 2 => 1}
 
-def fibonacci(term)
+def fibonacci2(term)
   return @@cache[1] if term == 1
   return @@cache[2] if term == 2
-  @@cache[term - 2] = fibonacci(term - 2) if !@@cache.has_key?(term - 2)
-  @@cache[term - 1] = fibonacci(term - 1) if !@@cache.has_key?(term - 1)
+  @@cache[term - 2] = fibonacci2(term - 2) if !@@cache.has_key?(term - 2)
+  @@cache[term - 1] = fibonacci2(term - 1) if !@@cache.has_key?(term - 1)
   return @@cache[term - 2] + @@cache[term - 1]
 end
 
-fibonacci(252)
+puts fibonacci2(input)
