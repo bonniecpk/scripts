a0f55bfc192798dbe9169ee9a1f7414b73472c25
bonniecpk <poki.developer@gmail.com>
2014-07-05 01:16:59 -0400

    using command line argument

--- ruby/power_set.rb
+++ ruby/power_set.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 # n = 0 returns [[], [0]]
 # n = 1 returns [[], [0], [1], [0,1]]
 # n = 2 returns [[], [0], [1], [2], [0,1], [0,2], [1,2], [0,1,2]]
@@ -5,9 +7,9 @@
 def power_set(n)
   return [[], [0]] if n == 0
   n_minus_1_set = power_set(n-1) if n > 0
-  n_minus_1_set + n_minus_1_set.map { |set| set.clone << n }
+  (n_minus_1_set + n_minus_1_set.map { |set| set.clone << n }).sort_by { |set| set.size }
 end
 
-puts power_set(0).inspect
-puts power_set(1).inspect
-puts power_set(2).inspect
+answer = power_set(ARGV[0].to_i)
+puts "Power set size = #{answer.size}"
+puts answer.inspect
