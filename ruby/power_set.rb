2d20150cdbe8915fb78dd1431a50bd4b4d6122ff
bonniecpk <poki.developer@gmail.com>
2014-07-05 00:59:45 -0400

    power set that takes infinite time

--- ruby/power_set.rb
+++ ruby/power_set.rb
@@ -1,25 +1,13 @@
-# Fibonacci sequence:
-# 1, 1, 2, 3, 5, 8, 13....
-def fibonacci(term)
-  return 1 if term == 1
-  return 1 if term == 2
-  return fibonacci(term - 2) + fibonacci(term - 1)
+# n = 0 returns [[], [0]]
+# n = 1 returns [[], [0], [1], [0,1]]
+# n = 2 returns [[], [0], [1], [2], [0,1], [0,2], [1,2], [0,1,2]]
+# etc
+def power_set(n)
+  return [[], [0]] if n == 0
+  n_minus_1_set = power_set(n-1) if n > 0
+  n_minus_1_set + n_minus_1_set.map { |set| set.clone << n }
 end
 
-puts fibonacci(4)
-
-
-
-# Fibonacci sequence:
-# 1, 1, 2, 3, 5, 8, 13....
-@@cache = {1 => 1, 2 => 1}
-
-def fibonacci(term)
-  return @@cache[1] if term == 1
-  return @@cache[2] if term == 2
-  @@cache[term - 2] = fibonacci(term - 2) if !@@cache.has_key?(term - 2)
-  @@cache[term - 1] = fibonacci(term - 1) if !@@cache.has_key?(term - 1)
-  return @@cache[term - 2] + @@cache[term - 1]
-end
-
-fibonacci(252)
+puts power_set(0).inspect
+puts power_set(1).inspect
+puts power_set(2).inspect
